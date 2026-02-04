2515ead49b5c1987687261b1dbd2430b76dedd69
Pranav chaudhari <praanavchaudharii200415@gmail.com>
2026-02-04 10:24:23 +0530

    fix footer missing para and BG color

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,111 +1,104 @@
-import { Link } from "react-router-dom";
+import React from "react";
 
 function Footer() {
   return (
-    <footer className="container-fluid border-top mt-5 pt-5">
-      <div className="container">
-        <div className="row">
-
-          {/* COLUMN 1 */}
-          <div className="col-md-3 mb-4">
-            <h6 className="fw-normal mb-3">Company</h6>
-            <ul className="list-unstyled">
-              <li className="mb-2">
-                <Link to="/about" className="text-decoration-none text-muted">
-                  About
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link to="/product" className="text-decoration-none text-muted">
-                  Products
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link to="/pricing" className="text-decoration-none text-muted">
-                  Pricing
-                </Link>
-              </li>
-              <li className="mb-2">
-                <button className="btn btn-link p-0 text-muted text-decoration-none">
-                  Careers
-                </button>
-              </li>
-            </ul>
+    <footer style={{ backgroundColor: "rgb(250, 250, 250)" }}>
+      <div className="container border-top mt-5">
+        <div className="row mt-5">
+          <div className="col">
+            <img src="media/images/logo.svg" style={{ width: "50%" }} />
+            <p className="mt-3 text-muted">
+              &copy; 2010 - 2024, Not Zerodha Broking Ltd. All rights reserved.
+            </p>
           </div>
-
-          {/* COLUMN 2 */}
-          <div className="col-md-3 mb-4">
-            <h6 className="fw-normal mb-3">Support</h6>
-            <ul className="list-unstyled">
-              <li className="mb-2">
-                <Link to="/support" className="text-decoration-none text-muted">
-                  Contact us
-                </Link>
-              </li>
-              <li className="mb-2">
-                <button className="btn btn-link p-0 text-muted text-decoration-none">
-                  Support portal
-                </button>
-              </li>
-              <li className="mb-2">
-                <button className="btn btn-link p-0 text-muted text-decoration-none">
-                  Downloads & resources
-                </button>
-              </li>
-            </ul>
+          <div className="col">
+            <p>Company</p>
+            <a href=""style={{textDecoration:"none"}}>About</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>Products</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>Pricing</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>Referral programme</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>Careers</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>Zerodha.tech</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>Press & media</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>Zerodha cares (CSR)</a>
+            <br />
           </div>
-
-          {/* COLUMN 3 */}
-          <div className="col-md-3 mb-4">
-            <h6 className="fw-normal mb-3">Account</h6>
-            <ul className="list-unstyled">
-              <li className="mb-2">
-                <Link to="/signup" className="text-decoration-none text-muted">
-                  Open an account
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link to="/login" className="text-decoration-none text-muted">
-                  Login
-                </Link>
-              </li>
-            </ul>
+          <div className="col">
+            <p>Support</p>
+            <a href=""style={{textDecoration:"none"}}>Contact</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>Support portal</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>Z-Connect blog</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>List of charges</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>Downloads & resources</a>
+            <br />
           </div>
-
-          {/* COLUMN 4 */}
-          <div className="col-md-3 mb-4">
-            <h6 className="fw-normal mb-3">More</h6>
-            <ul className="list-unstyled">
-              <li className="mb-2">
-                <a
-                  href="https://zerodha.tech"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-decoration-none text-muted"
-                >
-                  Zerodha.tech
-                </a>
-              </li>
-              <li className="mb-2">
-                <a
-                  href="https://twitter.com/zerodhaonline"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-decoration-none text-muted"
-                >
-                  Twitter
-                </a>
-              </li>
-            </ul>
+          <div className="col">
+            <p>Account</p>
+            <a href=""style={{textDecoration:"none"}}>Open an account</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>Fund transfer</a>
+            <br />
+            <a href=""style={{textDecoration:"none"}}>60 day challenge</a>
+            <br />
           </div>
-
         </div>
+        <div className="mt-5 text-muted" style={{ fontSize: "14px" }}>
+          <p>
+            Zerodha Broking Ltd.: Member of NSE​ &​ BSE – SEBI Registration no.:
+            INZ000031633 CDSL: Depository services through Zerodha Securities
+            Pvt. Ltd. – SEBI Registration no.: IN-DP-100-2015 Commodity Trading
+            through Zerodha Commodities Pvt. Ltd. MCX: 46025 – SEBI Registration
+            no.: INZ000038238 Registered Address: Zerodha Broking Ltd.,
+            #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School,
+            J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any
+            complaints pertaining to securities broking please write to
+            complaints@zerodha.com, for DP related to dp@zerodha.com. Please
+            ensure you carefully read the Risk Disclosure Document as prescribed
+            by SEBI | ICF
+          </p>
 
-        <hr />
+          <p>
+            Procedure to file a complaint on SEBI SCORES: Register on SCORES
+            portal. Mandatory details for filing complaints on SCORES: Name,
+            PAN, Address, Mobile Number, E-mail ID. Benefits: Effective
+            Communication, Speedy redressal of the grievances
+          </p>
 
-        <p className="text-center text-muted small mb-0">
-          © 2026 Zerodha Clone. Built for learning purposes.
-        </p>
+          <p>
+            Investments in securities market are subject to market risks; read
+            all the related documents carefully before investing.
+          </p>
+
+          <p>
+            "Prevent unauthorised transactions in your account. Update your
+            mobile numbers/email IDs with your stock brokers. Receive
+            information of your transactions directly from Exchange on your
+            mobile/email at the end of the day. Issued in the interest of
+            investors. KYC is one time exercise while dealing in securities
+            markets - once KYC is done through a SEBI registered intermediary
+            (broker, DP, Mutual Fund etc.), you need not undergo the same
+            process again when you approach another intermediary." Dear
+            Investor, if you are subscribing to an IPO, there is no need to
+            issue a cheque. Please write the Bank account number and sign the
+            IPO application form to authorize your bank to make payment in case
+            of allotment. In case of non allotment the funds will remain in your
+            bank account. As a business we don't give stock tips, and have not
+            authorized anyone to trade on behalf of others. If you find anyone
+            claiming to be part of Zerodha and offering such services, please
+            create a ticket here.
+          </p>
+        </div>
       </div>
     </footer>
   );
